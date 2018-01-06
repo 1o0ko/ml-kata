b422e3962a5729290828e2b85757c34392fb4908
1o0ko <wojciech.stokowiec@gmail.com>
2018-01-06 18:49:30 -0500

    autopep8

--- convolve.py
+++ convolve.py
@@ -13,28 +13,27 @@ if __name__ == '__main__':
     W = np.random.randn(k, k, c)
     b = np.random.randn(1, 1, 1)
 
-    n_w_c = (n_w - k + 2 * p)//s + 1
-    n_h_c = (n_h - k + 2 * p)//s + 1
+    n_w_c = (n_w - k + 2 * p) // s + 1
+    n_h_c = (n_h - k + 2 * p) // s + 1
 
     # create oupt matrix
     X_conv = np.zeros(shape=(n_w_c, n_h_c, c))
 
     # padd source image
-    X_pad = np.pad(X, [(p, p),(p, p),(0,0)], 'constant',  constant_values = 0)
+    X_pad = np.pad(X, [(p, p), (p, p), (0, 0)], 'constant', constant_values=0)
 
     # convolve
     for i in range(0, n_w_c):
         for j in range(0, n_h_c):
-            X_slice = X_pad[s*i:(k +s*i),s*j:(k+s*j),:]
+            X_slice = X_pad[s * i:(k + s * i), s * j:(k + s * j), :]
             assert X_slice.shape == (k, k, 3)
-            X_conv[i,j] = np.sum(np.multiply(X_slice, W)) + float(b)
+            X_conv[i, j] = np.sum(np.multiply(X_slice, W)) + float(b)
 
-
-    # Plotting 
+    # Plotting
     images = [
-        (X,      'image'), 
-        (X_pad,  'padded image'),  
-        (W,      'kernel'),
+        (X, 'image'),
+        (X_pad, 'padded image'),
+        (W, 'kernel'),
         (X_conv, 'convoled image')]
 
     f, axarr = plt.subplots(1, len(images), figsize=(20, 10))
