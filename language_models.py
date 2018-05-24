a1f1fe5b0e44a7de2d4ba7886915679686bffeea
1o0ko <wojciech.stokowiec@gmail.com>
2018-05-24 17:20:13 -0400

    cleaned lm-modules

--- language_models.py
+++ language_models.py
@@ -54,6 +54,9 @@ class TriGramModel(LanguageModel):
                 self.model[w1_w2][w3] /= total_count
 
     def sample(self, n=None):
+        '''
+        Gready sampling from a trigram model
+        '''
         text = [None, None]
         sentence_finished = False
 
@@ -77,61 +80,6 @@ class TriGramModel(LanguageModel):
 
         return ' '.join([t for t in text if t])
 
-
-import heapq
-
-
-class Beam(object):
-
-    def __init__(self, beam_width):
-        self.heap = list()
-        self.beam_width = beam_width
-
-    def add(self, prob, complete, prefix):
-        heapq.heappush(self.heap, (prob, complete, prefix))
-        if len(self.heap) > self.beam_width:
-            heapq.heappop(self.heap)
-
-    def __iter__(self):
-        return iter(self.heap)
-
-
-def beamsearch(
-        probabilities_function,
-        beam_width=10,
-        clip_len=-1):
-    '''
-    Performs beam search.
-    '''
-    prev_beam = Beam(beam_width)
-    prev_beam.add(1.0, False, ['<start>'])
-    while True:
-        curr_beam = Beam(beam_width)
-
-        # Add complete sentences that do not yet have the best probability to the current beam,
-        # the rest prepare to add more words to them.
-        for (prefix_prob, complete, prefix) in prev_beam:
-            if complete:
-                curr_beam.add(prefix_prob, True, prefix)
-            else:
-                # Get probability of each possible next word for the incomplete
-                # prefix.
-                for (next_prob, next_word) in probabilities_function(prefix):
-                    if next_word == '<end>':  # if next word is the end token then mark prefix as complete and leave out the end token
-                        curr_beam.add(prefix_prob * next_prob, True, prefix)
-                    else:  # if next word is a non-end token then mark prefix as incomplete
-                        curr_beam.add(
-                            prefix_prob * next_prob, False, prefix + [next_word])
-
-        (best_prob, best_complete, best_prefix) = max(curr_beam)
-        # if most probable prefix is a complete sentence or has a length that
-        # exceeds the clip length (ignoring the start token) then return it
-        if best_complete or len(best_prefix) - 1 == clip_len:
-            # return best sentence without the start token and together with
-            # its probability
-            return (best_prefix[1:], best_prob)
-
-        prev_beam = curr_beam
 
 
 if __name__ == '__main__':
