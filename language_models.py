24094e959fda356840df4ac59d59c8cdd3281c49
1o0ko <wojciech.stokowiec@gmail.com>
2018-05-25 17:31:59 -0400

    added prob estimate from the language model

--- language_models.py
+++ language_models.py
@@ -1,19 +1,39 @@
 # Beam Search
 
-from nltk.corpus import reuters
-from nltk import trigrams
-from collections import Counter, defaultdict
-
+import abc
+import operator
 import random
 
+from collections import Counter, defaultdict
+from functools import reduce
 
-class LanguageModel(object):
+from nltk.corpus import reuters
+from nltk import trigrams
 
+
+class LanguageModel(abc.ABC):
+    def __call__(self, sentence):
+        return self.prob(sentence)
+
+    @abc.abstractmethod
     def fit(self, corpus):
         pass
 
+    @abc.abstractmethod
     def sample(self, n=10):
         pass
+
+    @abc.abstractmethod
+    def _prob(self, sentence):
+        pass
+
+    def prob(self, sentence):
+        if not isinstance(sentence, list) and isinstance(sentence, str):
+            sentence = sentence.split()
+        else:
+            raise ValueError("Not a sting or list!")
+
+        return self._prob(sentence)
 
 
 class UnigramModel(LanguageModel):
@@ -28,6 +48,12 @@ class UnigramModel(LanguageModel):
     def sample(self, n=10):
         return ' '.join([self.sample_one_word() for _ in range(n)])
 
+    def _prob(self, sentence):
+        return reduce(operator.mul, [self.prob_word(word) for word in sentence])
+
+    def prob_word(self, word):
+        return self.model.get(word, min(self.model.values()))
+
     def sample_one_word(self):
         r = random.random()
         accumulator = .0
@@ -38,7 +64,14 @@ class UnigramModel(LanguageModel):
 
 
 class TriGramModel(LanguageModel):
+
+    def __init__(self):
+        self.unigram_lm = UnigramModel()
+
     def fit(self, corpus):
+        # for unigram backoff
+        self.unigram_lm.fit(corpus)
+
         # dict of dicts
         self.model = defaultdict(lambda: defaultdict(lambda: 0))
 
@@ -80,13 +113,37 @@ class TriGramModel(LanguageModel):
 
         return ' '.join([t for t in text if t])
 
+    def _prob(self, sentence):
+        ''' P([w1, w2, ..., wN])
+
+        Naive probability of a sentence with an unigram back-off
+        '''
+        probs = [
+            self.prob_trigram(t) for t in trigrams(sentence, pad_left=True)]
+
+        return reduce(operator.mul, probs)
+
+    def prob_trigram(self, trigram):
+        ''' P(t_3 | t_2, t1) '''
+        (t1, t2, t3) = trigram
+        items = self.model.get((t1, t2), None)
+        if items and t3 in items:
+            return items[t3]
+
+        # naive backoff maybe char level LM?
+        return self.unigram_lm(t3)
+
 
 
 if __name__ == '__main__':
-    unigram_lm = UnigramModel()
-    unigram_lm.fit(reuters)
-    unigram_lm.sample(5)
-
+    print('Training a LM')
     trigram_lm = TriGramModel()
     trigram_lm.fit(reuters)
-    trigram_lm.sample()
+
+    print('A sample from the model')
+    print(trigram_lm.sample())
+
+    p_1 = trigram_lm('This is a sentence')
+    p_2 = trigram_lm('Fishes like brown music')
+
+    assert p_1 > p_2
